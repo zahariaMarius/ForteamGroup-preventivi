a46daa77242909a00b8c398721f0d2e9e8090833
zaharia laurentiu jr marius <laurentiu.zaharia@edu.itspiemonte.it>
2017-11-02 00:00:09 +0100

    prova preventivi

--- js/scriptPreventivi.js
+++ js/scriptPreventivi.js
@@ -4,8 +4,8 @@
  * @Email:  laurentiu.zaharia@edu.itspiemonte.it
  * @Project: kebabCase
  * @Filename: scriptPreventivi.js
- * @Last modified by:   Toqir Nasir
- * @Last modified time: 2017-11-01T21:51:12+01:00
+ * @Last modified by:   Zaharia Laurentiu Jr Marius
+ * @Last modified time: 2017-11-01T23:33:56+01:00
  */
 
 "use strict";
@@ -658,3 +658,42 @@ app.controller('newProduct', function($scope) {
 
     window.addEventListener( 'scroll', scrollPage );
     trigger.addEventListener( 'click', function() { toggle( 'reveal' ); } );
+
+
+var textInput = document.querySelector('input');
+var inputWrap = textInput.parentElement ;
+var inputWidth = parseInt(getComputedStyle(inputWrap).width);
+var svgText = Snap('.line');
+var qCurve = inputWidth / 2;  // For correct curving on diff screen sizes
+var textPath = svgText.path("M0 0 " + inputWidth + " 0");
+var textDown = function(){
+    textPath.animate({d:"M0 0 Q" + qCurve + " 40 " + inputWidth + " 0"},150,mina.easeout);
+};
+var textUp = function(){
+  textPath.animate({d:"M0 0 Q" + qCurve + " -30 " + inputWidth + " 0"},150,mina.easeout);
+};
+var textSame = function(){
+  textPath.animate({d:"M0 0 " + inputWidth + " 0"},200,mina.easein);
+};
+var textRun = function(){
+  setTimeout(textDown, 200 );
+  setTimeout(textUp, 400 );
+  setTimeout(textSame, 600 );
+};
+
+(function(){
+    textInput.addEventListener('focus', function(){
+      var parentDiv = this.parentElement;
+      parentDiv.classList.add('active');
+      textRun();
+      this.addEventListener('blur', function(){
+        var rg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.]{3,9})+\.([A-Za-z]{2,4})$/;
+        this.value == 0 ? parentDiv.classList.remove('active') : null;
+        !rg.test(this.value) && this.value != 0 ?
+         (parentDiv.classList.remove('valid'), parentDiv.classList.add('invalid'), parentDiv.style.transformOrigin="center")
+         : rg.test(this.value) && this.value != 0 ?
+        (parentDiv.classList.add('valid'), parentDiv.classList.remove('invalid'), parentDiv.style.transformOrigin="bottom") :null;
+        });
+     parentDiv.classList.remove('valid', 'invalid')
+    });
+})();
