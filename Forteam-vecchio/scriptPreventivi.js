a33cec536a7d1ca05e4185898982efc6fe07ce5f
toqir nasir <toqir.nasir@edu.itspiemonte>
2017-11-22 15:04:24 +0100

    rtert

--- Forteam-vecchio/scriptPreventivi.js
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Zaharia Laurentiu Jr Marius
- * @Date:   2017-11-09T18:11:12+01:00
- * @Email:  laurentiu.zaharia@edu.itspiemonte.it
- * @Project: kebabCase
- * @Filename: scriptPreventivi.js
- * @Last modified by:   Zaharia Laurentiu Jr Marius
- * @Last modified time: 2017-11-09T18:11:12+01:00
- */
